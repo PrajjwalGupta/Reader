c9cd7fc7b71420ac5f39bf29bb153c798f94fd71
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2025-08-19 09:12:17 +0530

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Prajjwal on 19/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
